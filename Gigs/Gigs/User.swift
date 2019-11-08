c3624f41091cb199f497b273c49058377c6a93c2
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-07 21:56:52 -0500

    Initial model set up

---
+++ Gigs/Gigs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Christy Hicks on 11/7/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
