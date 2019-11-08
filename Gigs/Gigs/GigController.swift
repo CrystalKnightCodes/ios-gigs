44c400b2afa13a3ade7fd4bb13256866f3c461f0
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-07 22:10:21 -0500

    Added Gig Controller

---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,15 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Christy Hicks on 11/7/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+
+class GigController {
+    var bearer: Bearer?
+    let baseURL = "https://lambdagigs.vapor.cloud/api"
+    
+}
